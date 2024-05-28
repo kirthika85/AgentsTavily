0b55b012a91691e577e90c97c38a5c1a76e3b16a
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-27 21:08:37 -0700

    Update AgentsTavily.py

--- AgentsTavily.py
+++ AgentsTavily.py
@@ -19,6 +19,7 @@ from langchain.chains import create_retrieval_chain
 from langchain_core.messages import HumanMessage, AIMessage
 # Retriever tool
 from langchain.tools.retriever import create_retriever_tool
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 # Set the TAVILY API key directly
 os.environ['TAVILY_API_KEY'] = 'tavily_key'
@@ -36,59 +37,58 @@ splitter = RecursiveCharacterTextSplitter(
 )
 splitDocs = splitter.split_documents(docs)
 
-embedding = OpenAIEmbeddings(api_key = openai_api_key)
-vectorStore = FAISS.from_documents(docs, embedding=embedding)
-retriever = vectorStore.as_retriever(search_kwargs={"k": 3})
-
-model = ChatOpenAI(
-    model='gpt-3.5-turbo-1106',
-    temperature=0.7,
-    api_key = openapikey
-)
-
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a friendly assistant called Max."),
-    MessagesPlaceholder(variable_name="chat_history"),
-    ("human", "{input}"),
-    MessagesPlaceholder(variable_name="agent_scratchpad")
-])
-
-from langchain_community.tools.tavily_search import TavilySearchResults
-
-search = TavilySearchResults()
-
-retriever_tools = create_retriever_tool(
-    retriever,
-    "lcel_search",
-    "Use this tool when searching for information about Langchain Expression Language (LCEL)."
-)
-tools = [search, retriever_tools]
-
-agent = create_openai_functions_agent(
-    llm=model,
-    prompt=prompt,
-    tools=tools
-)
-
-agentExecutor = AgentExecutor(
-    agent=agent,
-    tools=tools
-)
-
-def process_chat(agentExecutor, user_input, chat_history):
-    response = agentExecutor.invoke({
-        "input": user_input,
-        "chat_history": chat_history
-    })
-    return response["output"]
-
 st.title("Document Search with RAG")
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 tavily_key = st.sidebar.text_input('Tavily API Key', type='password')
 if not openai_api_key.startswith('sk-'):
    st.warning('Please enter your OpenAI API key!', icon='⚠')
 if openai_api_key.startswith('sk-') and tavily_key:
-   if __name__ == '__main__':
+
+    embedding = OpenAIEmbeddings(api_key = openai_api_key)
+    vectorStore = FAISS.from_documents(docs, embedding=embedding)
+    retriever = vectorStore.as_retriever(search_kwargs={"k": 3})
+
+    model = ChatOpenAI(
+            model='gpt-3.5-turbo-1106',
+            temperature=0.7,
+            api_key = openai_api_key
+    )        
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a friendly assistant called Max."),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ])
+
+    search = TavilySearchResults()
+
+    retriever_tools = create_retriever_tool(
+        retriever,
+        "lcel_search",
+        "Use this tool when searching for information about Langchain Expression Language (LCEL)."
+    )
+    tools = [search, retriever_tools]
+
+    agent = create_openai_functions_agent(
+        llm=model,
+        prompt=prompt,
+        tools=tools
+    )
+
+    agentExecutor = AgentExecutor(
+        agent=agent,
+        tools=tools
+    )
+
+    def process_chat(agentExecutor, user_input, chat_history):
+        response = agentExecutor.invoke({
+            "input": user_input,
+            "chat_history": chat_history
+        })
+    return response["output"]
+
+    if __name__ == '__main__':
         chat_history = []
         while True:
           user_input = input("You: ")
